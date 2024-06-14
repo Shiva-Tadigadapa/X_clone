269a119cf420a9c47fce9248fe0d8a7cd57bf1f2
Shiva Tadigadapa <89725958+Shiva-Tadigadapa@users.noreply.github.com>
2024-06-14 15:26:06 +0530

    Update links of google auth

--- Server/Controllers/AuthControllers.js
+++ Server/Controllers/AuthControllers.js
@@ -11,6 +11,8 @@ const client = new OAuth2Client(CLIENT_ID);
 
 export const googleAuth = async (req, res) => {
     const { token } = req.body;
+        const response = await fetch('https://picsum.photos/1500/500');
+        const coverPhoto = response.url;
 
     try {
         // Verify the token
@@ -37,6 +39,7 @@ export const googleAuth = async (req, res) => {
                 handle, // Assign the generated handle
                 email,
                 profilePicture, // Assign the profile picture URL
+                coverPhoto
             });
             user = await user.save();
         }
